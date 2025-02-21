7c95674bdabdba6f3297535200a94e0a36d22765
Tabassum290 <keyatabassum2@gmail.com>
2025-02-21 10:15:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const port = process.env.port || 4000;
+const app = express();
+
+// middleswars
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ejql3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    const userCollection = client.db("Tasks").collection("users");
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+      const query = { email: user.email }
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+});
+app.get('/users',async (req, res) => {
+    const result = await userCollection.find() .toArray();
+    console.log(result);
+    res.send(result);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.connect();
+   await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.get('/',async(req,res)=>{
+    res.send('Server is Running')
+})
+app.listen(port,()=>{
+    console.log(`Server is running in ${port}`)
+})
